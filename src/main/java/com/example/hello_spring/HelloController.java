7f0f74e3917322b04a27cdf111da25a141a9494f
KimJayhyun <dnlwjem000@gmail.com>
2026-03-23 00:04:28 +0900

    feat: jpa를 이용한 data 적재

--- src/main/java/com/example/hello_spring/HelloController.java
+++ src/main/java/com/example/hello_spring/HelloController.java
@@ -1,7 +1,5 @@
 package com.example.hello_spring;
 
-import java.sql.PreparedStatement;
-
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,36 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    public HelloController(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public HelloController(UserRepository userRepository, JdbcTemplate jdbcTemplate) {
+        this.userRepository = userRepository;
     }
 
-    private final JdbcTemplate jdbcTemplate;
-    private final Object idLock = new Object();
+    private final UserRepository userRepository;
 
     @PostMapping("/users")
     public User createUser(@RequestBody User newUser) {
-        String sql = "INSERT INTO users(id, name, age) VALUES(?, ?, ?)";
-
-        Long finalId;
-        synchronized (idLock) {
-            Long nextId = jdbcTemplate.queryForObject("SELECT COALESCE(MAX(id), 0) + 1 FROM users",
-                    Long.class);
-            finalId = nextId;
-        }
-        newUser.setId(finalId);
-
-        jdbcTemplate.update(connection -> {
-            PreparedStatement ps = connection.prepareStatement(sql);
-
-            ps.setLong(1, newUser.getId());
-            ps.setString(2, newUser.getName());
-            ps.setInt(3, newUser.getAge());
-
-            return ps;
-        });
-
-        return newUser;
+        User savedUser = userRepository.save(newUser);
+        return savedUser;
     }
 
     @GetMapping("/hello")
@@ -54,28 +32,12 @@ public class HelloController {
     @GetMapping("/user")
     public User getUser() {
         User user = new User("test", 20);
-
         return user;
     }
 
     @GetMapping("/user/{id}")
     public User getUserById(@PathVariable("id") Long id) {
-        String sql = "SELECT id, name, age FROM users WHERE id = ?";
-
-        // User user = jdbcTemplate.queryForObject(sql, new RowMapper<User>() {
-        // @Override
-        // public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-
-        // User user = new User(rs.getString("name"), rs.getInt("age"));
-        // user.setId(rs.getLong("id"));
-        // return user;
-        // }
-        // }, id);
-        return jdbcTemplate.queryForObject(sql, (rs, rowNum) -> {
-            User user = new User(rs.getString("name"), rs.getInt("age"));
-            user.setId(rs.getLong("id"));
-            return user;
-        }, id);
+        return userRepository.findById(id).orElse(null);
     }
 
     // @GetMapping("/users")
